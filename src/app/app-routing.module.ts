3f2ee05f5c728bebd35bb75495b077803291db61
aravind kesavarapu <kesavarapu.aravind@gmail.com>
2020-04-20 12:11:54 +0530

    1st assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [
+  { path: 'Home',component: HomeComponent },
+  { path: 'About',component: AboutComponent},
+  { path: 'ContactUs',component: ContactusComponent},
+  { path: 'Register', component: RegisterComponent},
+  { path: 'Login', component: LoginComponent}
 
-
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextComponent } from './text/text.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactusComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
